3c597afb96ffb809a7a96bf2e49a003f324b7fcb
dblinkhorn <dblinkhorn@gmail.com>
2020-08-14 01:37:56 -0700

    just more typos and formatting, yawn

--- wheel_of_fortune.py
+++ wheel_of_fortune.py
@@ -4,7 +4,7 @@ import random
 import time
 import sys
 
-# dictionary to store puzzles for program to choose from
+# dictionary to store puzzles for select_puzzle() to choose from
 puzzles = {
     1: "TODAY IS THE DAY",
     2: "EVERYONE LOVES PIZZA",
@@ -25,7 +25,7 @@ def select_puzzle(puzzles):
     global letter_board
     puzzle = random.choice(list(puzzles.values()))
 
-    # list to store blank spaces generated from randomly selection puzzle from puzzles dictionary
+    # list to store blank spaces generated from randomly selected puzzle from puzzles dictionary
     letter_board = []
 
     # loop to build number of blanks on letter board from selected puzzle
@@ -49,14 +49,15 @@ used_letters = []
 
 
 # function to reveal successfully guessed letters on letter board
+# only accepts already validated guess (letter is in the puzzle)
 def reveal_letters(valid_guess):
     # for each hidden letter in the board (number based on characters in puzzle)
     for blank in range(len(letter_board)):
         # if that letter is equal to the player's guess
         if puzzle[blank] == valid_guess:
-            # then change that blank to that letter, revealing the successful guess on the board
+            # then change that blank to that letter, revealing the successfully guessed letter(s) on the letter board
             letter_board[blank] = valid_guess
-    # prints out the game board and reveals guessed letters
+    # prints out the letter board and reveals guessed letters
     print("        Current Letters\n"
           "        ---------------")
     print("        ", end="")
@@ -78,14 +79,14 @@ int_to_str = {
 }
 
 
-# function to spin the wheel
+# function that controls all of the spin wheel logic
 def spin_wheel():
     print("\nSpinning...\n")
 
     # adds a two second delay for dramatic anticipation!
     time.sleep(2)
 
-    # variable spin set to a random value from wheel dictionary
+    # variable spin set to a random value from wheel dictionary (random wheel slice)
     spin = random.choice(list(wheel.values()))
 
     # if wheel lands on bankrupt, player losses all their money
@@ -95,7 +96,7 @@ def spin_wheel():
         # return None to get out of function and bring back main menu
         return
 
-        # Below, "\033[1m" starts bold text in the terminal, "\033[0m" finishes it
+    # below, "\033[1m" begins bold text in the terminal, "\033[0m" ends it
     print(f"    The wheel landed on: \033[1m${spin}\033[0m.\n")
 
     # set player_money and used_letters to be preserved outside function scope
@@ -106,7 +107,7 @@ def spin_wheel():
     check_input = True
     while check_input:
         try:
-            # spin menu
+            # spin menu options
             spin_choice = int(input("Choose an action:\n"
                                     "1. Guess a consonant\n"
                                     "2. Buy a vowel (Costs $500)\n"
@@ -124,11 +125,13 @@ def spin_wheel():
                     elif guess in used_letters:
                         print("\n    You have already used that letter. Please try again.\n")
                         continue
+                    # if the consonant hasn't been used then add it to the used_letters now
                     elif guess not in used_letters:
                         used_letters.append(guess)
                         consonant_prompt = False
                         check_input = False
             if spin_choice == 2:
+                # checks to make sure user has the $500 to buy a vowel
                 if player_money < 500:
                     print("\n    Sorry, you don't have enough money to purchase a vowel yet. Try guessing a consonant "
                           "instead.\n")
@@ -144,6 +147,7 @@ def spin_wheel():
                     elif guess in used_letters:
                         print("\n    You have already used that letter. Please try again.\n")
                         continue
+                    # if the vowels hasn't been used then add it to the used_letters now
                     elif guess not in used_letters:
                         used_letters.append(guess)
                         vowel_prompt = False
@@ -161,10 +165,10 @@ def spin_wheel():
     # if user input passes all tests in spin_wheel(), set as valid_guess
     valid_guess = guess
 
-    # variable to store count of guessed letters in phrase
+    # variable to store the count of correctly guessed letters in the puzzle
     number_correct = puzzle.count(valid_guess)
 
-    # if statements to reveal success of user letter
+    # if statements to determine the outcome of the user's guess
     if number_correct > 1:
         print(f"\n    Congratulations! There are {str(int_to_str[number_correct])} {valid_guess.upper()}'s" + " in the "
               "puzzle.\n")
@@ -175,23 +179,23 @@ def spin_wheel():
               "puzzle.\n")
     reveal_letters(valid_guess)
 
-    # print this if guess was a consonant
+    # print this if the guess was a consonant
     if valid_guess in consonants:
-        # winnings equals spin value times number of guessed letters in phrase
+        # winnings equals wheel slice spin value times the number of correctly guessed letters in the puzzle
         player_money = player_money + (spin * number_correct)
-        # tell user how much they won this round and current total winnings
+        # tells the user how much they won this round and their current total winnings
         print(f"\n    You won \033[1m${spin * number_correct}\033[0m this round. You currently have \033[1m"
               f"${player_money}\033[0m "
               f"total.")
 
-    # print this if guess was a vowel
+    # print this if the guess was a vowel
     elif valid_guess in vowels:
         player_money = player_money - 500
         print(f"\n    You purchased the vowel '{valid_guess}' for \033[1m$500\033[0m. You currently have \033[1m"
               f"${player_money}\033[0m total.")
 
 
-# function to confirm user wants to quit program
+# function to confirm if the user wants to quit
 def menu_quit():
     invalid_input = True
     while invalid_input:
@@ -206,16 +210,17 @@ def menu_quit():
             invalid_input = False
 
 
-# function to display main menu of game
+# function to display the main menu of the game
 def main_menu():
-    # loop to check main menu input
-    while True:
+    # loop to check the main menu inputs
+    main_loop = True
+    while main_loop:
         # sets variables player_money and puzzle to be preserved outside of function scope
         global used_letters
         global player_money
         global puzzle
         try:
-            # main menu
+            # main menu options
             menu_choice = int(input("\nChoose an action:\n"
                                     "1. Spin the wheel\n"
                                     "2. Solve the puzzle\n"
@@ -223,7 +228,7 @@ def main_menu():
                                     "\nEnter choice: "))
             if menu_choice == 1:
                 spin_wheel()
-            # exits the game
+            # logic for user guessing the puzzle
             elif menu_choice == 2:
                 puzzle_guess = input("Enter your guess (include spaces, not case-sensitive): ").upper()
                 if puzzle_guess == puzzle:
@@ -231,20 +236,30 @@ def main_menu():
                     player_money += 1000
                     print(f"\n    You won \033[1m$1000\033[0m this round for solving the puzzle and earned a "
                           f"total of \033[1m${player_money}\033[0m this game.")
+                    # reset player money for next round
                     player_money = 0
+                    # reset used letters for next round
                     used_letters = []
+
+                    # loop to check play again prompt input
                     confirm_replay = True
                     while confirm_replay:
-                        # variable to store if user wants to play another game or quit
+                        # variable to store whether user wants to play another round or quit
                         play_again = input("\nWould you like to play again? Enter (Y)es or (N)o: ").upper()
                         while play_again not in ('Y', 'N'):
                             print("\n    Invalid selection. Enter 'Y' or 'N'.")
                             break
+                        # if they want to play another round
                         if play_again == 'Y':
+                            # reset the puzzle to a random puzzle from the puzzles dictionary for the new round
                             puzzle = select_puzzle(puzzles)
                             print("\nHere is the next puzzle...\n")
+                            # display the new letter board
                             print(*letter_board)
+                            # activate the main menu again
                             main_menu()
+
+                        # logic if the user doesn't want to play again
                         if play_again == 'N':
                             check_quit = False
                             while not check_quit:
@@ -255,14 +270,22 @@ def main_menu():
                                 if confirm_quit == 'Y':
                                     print("\n    Thank you for playing. Goodbye!")
                                     sys.exit()
+
+                                # if they say they don't want to quit after all,
+                                # then it's as if they had said yes to original prompt to play again
+                                # resets the puzzle anew, displays the new letter board, and activates the main menu
                                 elif confirm_quit == 'N':
                                     puzzle = select_puzzle(puzzles)
                                     print("\nHere is the next puzzle...\n")
                                     print(*letter_board)
                                     main_menu()
+
+                # if the user's guess is not correct
                 elif puzzle_guess != puzzle:
                     print("\n    Sorry, that is not the correct answer.")
                     continue
+
+            # if the user wants to quit
             elif menu_choice == 3:
                 menu_quit()
 
@@ -274,7 +297,7 @@ def main_menu():
             continue
 
 
-# calls function to select random puzzle from the puzzles dictionary
+# calls function to select a random puzzle from the puzzles dictionary
 select_puzzle(puzzles)
 
 # variable to store current player winnings
@@ -282,7 +305,7 @@ player_money = 0
 
 # dictionary to store each slice of the wheel
 wheel = {
-    # activates a function that removes all the player's money
+    # activates a function that resets player's winnings to $0
     "Bankrupt": "BANKRUPT",
     "$100": 100,
     "$200": 200,
@@ -298,12 +321,13 @@ wheel = {
 }
 
 # variables set to contain all possible letters
-# each list used to validate user input in spin_wheel()
+# each list is used to validate the user input in spin_wheel()
 consonants = list("BCDFGHJKLMNPQRSTVWXYZ")
 vowels = list("AEIOU")
 
 # welcome message/program title
 print("\nWelcome to the WHEEL OF FORTUNE!\n")
+
 # explain game rules
 print("    RULES\n"
       "    -----\n"
@@ -313,7 +337,7 @@ print("    RULES\n"
       "    4. You may attempt to solve the puzzle at any time. ($1000 bonus if successful)\n"
       "\n    \033[1mGood luck!\033[0m")
 
-# prints blank puzzle for user
+# prints blank, unrevealed puzzle for the user
 print("\nHere is the puzzle...\n")
 print(*letter_board)
 
